9176ec343a98df37b073d360e023009f1a726259
lexraider <lex_85@hotmail.com>
2014-09-18 23:13:26 +0800

    Assignment 2 Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2
+## Cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Creates a "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+    # Get matrix dimension
+    r <- nrow(x)
+    c <- ncol(x)
+    
+    # Create inverse matrix
+    i <- matrix(nrow = r, ncol = c)
+  
+    set <- function(y) {
+        x <<- y
+    
+        # Get new matrix dimension
+        newr <- nrow(y)
+        newc <- ncol(y)
+        
+        # Clear the inverse matrix & initialize with new dimension
+        i <<- NULL
+        i <<- matrix(nrow = newr, ncol = newc)
+    }
+  
+    get <- function() x
+  
+    setinverse <- function(inverse) i <<- inverse
+  
+    getinverse <- function() i
+  
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the above matrix.
+## If the inverse has already been calculated (matrix remains unchanged),
+## it should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.na(i[1,1])) {
+        message("getting cached data")
+        return(i)
+    }
+  
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
